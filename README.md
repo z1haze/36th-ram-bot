3130c30d8938d69c8187b465e0450f88fa69ed3f
Stephen Hendricks <stephen.hendricks87@gmail.com>
2021-09-11 10:16:53 -0700

    remove welcome role logic and use default discord rule acceptance

--- README.md
+++ README.md
@@ -8,23 +8,10 @@ Role Assignment Management (RAM) Bot
 What It Does
 ---
 
-- [x] Assigns a role (configurable) to a user when they join the server
 - [x] Utilizes a custom message format for role assignment messages
-- [x] Handles gating newly joined users through a series of welcome channels
  
 Usage Overview
 ---
- 
-#### User join role
-When a new user joins the server, the bot can automatically assign a role to this user.
-The role ID needs to be set in the `.env` file under `WELCOME_ROLE_IDS` (first entry)
-
-#### Gated Welcome Channels
-When a new user joins the server, you can have visibility locked to a set of welcome channels setup in your `.env` file
-under `WELCOME_CHANNEL_IDS` as a comma separated list. You will also need `WELCOME_ROLE_IDS` defined in your `.env` file
-as well (one for each welcome channel). The bot will navigate the user through the welcome channels, by adding/removing the
-appropriate roles to show visibility to the correct welcome channel. Upon complete of the welcome channel flow, the bot will
-assign a role to the user, defined in your `.env` file under `GUEST_ROLE_ID`.
 
 #### Role Assignment Messages (RAM)
 This bot will also handle role assignments for channels listed in the `RAM_CHANNELS` of the `.env` file.
@@ -59,9 +46,5 @@ NODE_ENV=
 SENTRY_DSN=
 BOT_TOKEN=
 GUILD_ID=
-WELCOME_CHANNEL_IDS=
-WELCOME_ROLE_IDS=
-WELCOME_EMOJI_ID=
 RAM_CHANNEL_IDS=
-GUEST_ROLE_ID=
 ```
--- index.js
+++ index.js
@@ -1,32 +1,112 @@
 require('dotenv').config();
+require('./util/sentry').init();
 
+const {Client} = require('discord.js');
 const translateMessage = require('./util/translate-message');
 const helpers = require('./util/role-helpers');
-const monitor = require('./util/sentry');
-const {Client} = require('discord.js');
 
 const BOT_TOKEN = process.env.BOT_TOKEN;
 const GUILD_ID = process.env.GUILD_ID;
 
-let GUILD;
-let GUEST_ROLE;
-let WELCOME_EMOJI;
-
-const WELCOME_CHANNEL_IDS = process.env.WELCOME_CHANNEL_IDS.split(',');
-const WELCOME_ROLE_IDS = process.env.WELCOME_ROLE_IDS.split(',');
-const RAM_CHANNEL_IDS = process.env.RAM_CHANNEL_IDS.split(',');
-
-const WELCOME_ROLES = new Map();
-const WELCOME_CHANNELS = new Map();
 const RAM_CHANNELS = new Map();
-
-monitor.init();
 
 const client = new Client({
     partials: ['REACTION']
 });
 
-const processMessage = (message, oldMessage = null) => {
+client.on('ready', async () => {
+    // eslint-disable-next-line no-console
+    console.info(`Logged in as ${client.user.tag}!`);
+
+    const guild = client.guilds.cache.get(GUILD_ID);
+
+    if (!guild) {
+        throw new Error(`Incorrect guild id: ${GUILD_ID}`);
+    }
+
+    // initialize RAM channels and messages
+    process.env.RAM_CHANNEL_IDS.split(',').forEach((channelId) => {
+        const channel = guild.channels.cache.get(channelId);
+
+        if (channel) {
+            RAM_CHANNELS.set(channelId, channel);
+
+            // setup listeners on each RAM message in the channel
+            channel.messsages.fetch()
+                .then((messages) => messages.each(processMessage));
+        } else {
+            throw new Error(`RAM channel not found matching id: ${channelId}`);
+        }
+    });
+});
+
+/**
+ * After the bot adds a new RAM message, process the message to add all appropriate reactions
+ */
+client.on('message', (message) => {
+    // the only messages we care about are bot messages
+    if (!message.author.bot) {
+        return;
+    }
+
+    // handle messages for RAM channels
+    if (RAM_CHANNELS.has(message.channel.id)) {
+        return processMessage(message);
+    }
+});
+
+/**
+ * After the bot updates the RAM message, process to message to add or remove any necessary reactions
+ */
+client.on('messageUpdate', (oldMessage, message) => {
+    // the only messages we care about are bot messages
+    if (!message.author.bot) {
+        return;
+    }
+
+    // handle messages for RAM channels
+    if (RAM_CHANNELS.has(message.channel.id)) {
+        return processMessage(message, oldMessage);
+    }
+});
+
+/**
+ * Give a role to a member after the add their reaction
+ */
+client.on('messageReactionAdd', (messageReaction, user) => {
+    // the only messages we care about are user messages
+    if (user.bot) {
+        return;
+    }
+
+    if (RAM_CHANNELS.has(messageReaction.message.channel.id)) {
+        const member = messageReaction.message.guild.members.cache.get(user.id);
+
+        handleUserReaction(messageReaction, member);
+    }
+});
+
+/**
+ * Remove a role from a member after they remove their reaction
+ */
+client.on('messageReactionRemove', (messageReaction, user) => {
+    if (user.bot) {
+        return;
+    }
+    
+    if (RAM_CHANNELS.has(messageReaction.message.channel.id)) {
+        const member = messageReaction.message.guild.members.cache.get(user.id);
+
+        handleUserReaction(messageReaction, member, true);
+    }
+});
+
+client.login(BOT_TOKEN);
+
+/**
+ * Setup emoji reactions for a RAM channel message
+ */
+function processMessage (message, oldMessage = null) {
     // translate message into emoji/role pairs, and get a list of reactions to remove
     const {lines, toRemove} = translateMessage(message, oldMessage);
 
@@ -47,188 +127,25 @@ const processMessage = (message, oldMessage = null) => {
             }
         }
     }
-};
+}
 
-const handleUserReaction = (messageReaction, member, remove = false) => {
+/**
+ * Add/Remove roles from users based on their message reaction
+ *
+ * @param messageReaction
+ * @param member
+ * @param remove
+ */
+function handleUserReaction (messageReaction, member, remove = false) {
     const {translations} = translateMessage(messageReaction.message);
 
     if (translations.has(messageReaction.emoji.name)) {
         const role = translations.get(messageReaction.emoji.name);
 
         if (remove) {
-            helpers.takeRole(GUILD, member, role);
+            helpers.takeRole(messageReaction.message.guild, member, role);
         } else {
-            helpers.giveRole(GUILD, member, role);
+            helpers.giveRole(messageReaction.message.guild, member, role);
         }
     }
-};
-
-client.on('ready', async () => {
-    // eslint-disable-next-line no-console
-    console.info(`Logged in as ${client.user.tag}!`);
-
-    GUILD = client.guilds.cache.get(GUILD_ID);
-
-    if (!GUILD) {
-        throw new Error(`Incorrect guild id: ${GUILD_ID}`);
-    }
-
-    // set the correct join role
-    GUEST_ROLE = GUILD.roles.cache.get(process.env.GUEST_ROLE_ID);
-
-    if (!GUEST_ROLE) {
-        throw new Error(`Guest role not found matching id: ${process.env.GUEST_ROLE_ID}`);
-    }
-
-    // set the correct welcome emoji
-    WELCOME_EMOJI = GUILD.emojis.cache.get(process.env.WELCOME_EMOJI_ID);
-
-    if (!WELCOME_EMOJI) {
-        throw new Error(`Welcome emoji not found matching id: ${process.env.WELCOME_EMOJI_ID}`);
-    }
-
-    // set the correct welcome roles
-    WELCOME_ROLE_IDS.forEach((roleId) => {
-        const role = GUILD.roles.cache.get(roleId);
-
-        if (role) {
-            WELCOME_ROLES.set(roleId, role);
-        } else {
-            throw new Error(`Welcome role not found matching id: ${roleId}`);
-        }
-    });
-
-    // set the correct welcome channels
-    WELCOME_CHANNEL_IDS.forEach((channelId) => {
-        const channel = GUILD.channels.cache.get(channelId);
-
-        if (channel) {
-            WELCOME_CHANNELS.set(channelId, channel);
-        } else {
-            throw new Error(`Welcome channel not found matching id: ${channelId}`);
-        }
-    });
-
-    // set the correct RAM channels
-    RAM_CHANNEL_IDS.forEach((channelId) => {
-        const channel = GUILD.channels.cache.get(channelId);
-
-        if (channel) {
-            RAM_CHANNELS.set(channelId, channel);
-        } else {
-            throw new Error(`RAM channel not found matching id: ${channelId}`);
-        }
-    });
-
-    // initialize bot reactions on welcome channels
-    for (const [, channel] of WELCOME_CHANNELS) {
-        channel.messages.fetch()
-            .then((messages) => {
-                for (const [, message] of messages) {
-                    message.react(WELCOME_EMOJI);
-                }
-            });
-    }
-
-    // initialize bot reactions on RAM channels
-    for (const [, channel] of RAM_CHANNELS) {
-        channel.messages.fetch()
-            .then((messages) => {
-                for (const [, message] of messages) {
-                    processMessage(message);
-                }
-            });
-    }
-});
-
-client.on('guildMemberAdd', async (member) => {
-    if (WELCOME_ROLES.size > 0) {
-        const role = WELCOME_ROLES.values().next().value;
-
-        if (!member.roles.cache.has(role.id)) {
-            await member.roles.add(role);
-        }
-    }
-});
-
-client.on('message', (message) => {
-    // the only messages we care about are bot messages
-    if (!message.author.bot) {
-        return;
-    }
-
-    // handle messages for welcome channels
-    if (WELCOME_CHANNELS.has(message.channel.id)) {
-        return message.react(WELCOME_EMOJI);
-    }
-
-    // handle messages for RAM channels
-    if (RAM_CHANNELS.has(message.channel.id)) {
-        return processMessage(message);
-    }
-});
-
-client.on('messageUpdate', (oldMessage, message) => {
-    // the only messages we care about are bot messages
-    if (!message.author.bot) {
-        return;
-    }
-
-    // handle messages for welcome channels
-    if (WELCOME_CHANNELS.has(message.channel.id)) {
-        return message.react(WELCOME_EMOJI);
-    }
-
-    // handle messages for RAM channels
-    if (RAM_CHANNELS.has(message.channel.id)) {
-        return processMessage(message, oldMessage);
-    }
-});
-
-client.on('messageReactionAdd', (messageReaction, user) => {
-    // the only messages we care about are user messages
-    if (user.bot) {
-        return;
-    }
-
-    // handle messages for welcome channels
-    if (WELCOME_CHANNELS.has(messageReaction.message.channel.id)) {
-        const index = WELCOME_CHANNEL_IDS.indexOf(messageReaction.message.channel.id);
-        const roleId = WELCOME_ROLE_IDS[index];
-        const role = WELCOME_ROLES.get(roleId);
-
-        const member = GUILD.members.cache
-            .get(user.id);
-
-        member.roles.remove(role);
-
-        // if there is another welcome channel
-        if (index < WELCOME_CHANNELS.size - 1) {
-            const roleId = WELCOME_ROLE_IDS[index + 1];
-            const role = WELCOME_ROLES.get(roleId);
-
-            member.roles.add(role);
-        } else {
-            // no more welcome channels, finished
-            member.roles.add(GUEST_ROLE);
-        }
-    }
-
-    if (RAM_CHANNELS.has(messageReaction.message.channel.id)) {
-        const member = GUILD.members.cache
-            .get(user.id);
-
-        handleUserReaction(messageReaction, member);
-    }
-});
-
-client.on('messageReactionRemove', (messageReaction, user) => {
-    if (RAM_CHANNELS.has(messageReaction.message.channel.id)) {
-        const member = GUILD.members.cache
-            .get(user.id);
-
-        handleUserReaction(messageReaction, member, true);
-    }
-});
-
-client.login(BOT_TOKEN);
+}
